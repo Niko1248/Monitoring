5b5bb3e5fe5c130e890cc695e530129c44214fb2
Niko1248 <Sirius04@bk.ru>
2022-11-20 10:22:53 +0300

    Добавил попап

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 window.onload = function (){
 	const state = document.querySelectorAll('.state');
+	const system = document.querySelectorAll('.table__grid');
+
 	state.forEach(el => {
 		if (el.innerHTML == 'Авария'){
 			el.classList.add('state_off');
@@ -7,5 +9,15 @@ window.onload = function (){
 			el.classList.add('state_on');
 		}
 	});
+	system.forEach(item => {
+		item.addEventListener("click", () => {
+			let popup = item.querySelector('.popup__bg');
+			popup.classList.add('popup__on');
+			let popupClose = item.querySelector('.close');
+			popupClose.addEventListener('click', () => {
+				popup.classList.remove('popup__on');
+			})
+		})
+	})
 }
 
--- js/renderSystems.js
+++ js/renderSystems.js
@@ -6,23 +6,40 @@ async function getSystems() {
   //получаем массив из json в котором мы определили массив
   //обязательно через await,необходимо достоверно получить данные
   const response = await fetch('./js/systems.json')
-  //присваиваем хначение массива переменной
+  //присваиваем значение массива переменной
   const systemsArray = await response.json()
   //теперь передаем этоту переменную с массивом в другую функцию
 	renderSystems(systemsArray)
 }
 
 function renderSystems(systemsArray) {
-  //пербираем все элементы в массиве и вставляем их в шаблон
+  //перебираем все элементы в массиве и вставляем их в шаблон
   systemsArray.forEach(function (item) {
-    const systemHTML = `<div class="table__item table__grid">
-												<div>${item.id}</div>
-												<div>${item.number}</div>
-												<div>${item.priority}</div>
-												<div>${item.correspondent}</div>
-												<div>${item.type}</div>
-												<div class="state">${item.state}</div>
-												</div>`
+    const systemHTML = `<div class="table__grid">
+													<div class="popup__bg">
+														<div class="popup">
+															<div class="close">
+																<a class="close__img">
+																	<img src="./img/close.png" alt="" />
+																</a>
+															</div>
+															<div class="popup__content">
+																<div>Трасса: </div>
+																<div>Каналы: </div>
+																<div>Что-нибудь еще: </div>
+															</div>
+														</div>					
+													</div>
+													<div class="table__item">
+														<div>${item.id}</div>
+														<div>${item.number}</div>
+														<div>${item.priority}</div>
+														<div>${item.correspondent}</div>
+														<div>${item.type}</div>
+														<div class="state">${item.state}</div>
+													</div>
+												</div>
+												`
 		    //вставляем шаблон в страницу(отрисовываем)
     systemsContainer.insertAdjacentHTML('beforeend', systemHTML)
   })
