895613140d54dccb40b52e806890b4d2da508f9d
mr-usackiy <77061592+mr-usackiy@users.noreply.github.com>
2022-11-20 20:23:09 +0300

    переделал внешний вид и добавил прелоадер для геев

--- js/renderSystems.js
+++ js/renderSystems.js
@@ -1,6 +1,6 @@
-const systemsContainer = document.querySelector('#table-systems');
+const systemsContainer = document.querySelector('#table-systems')
 
-getSystems();
+getSystems()
 
 async function getSystems() {
   //получаем массив из json в котором мы определили массив
@@ -9,35 +9,55 @@ async function getSystems() {
   //присваиваем значение массива переменной
   const systemsArray = await response.json()
   //теперь передаем этоту переменную с массивом в другую функцию
-	renderSystems(systemsArray)
+  renderSystems(systemsArray)
 }
 
 function renderSystems(systemsArray) {
   //перебираем все элементы в массиве и вставляем их в шаблон
   systemsArray.forEach(function (item) {
     const systemHTML = `<div class="table__grid">
-													<div class="table__item">
-														<div>${item.id}</div>
-														<div class="number">${item.number}</div>
-														<div>${item.priority}</div>
-														<div>${item.correspondent}</div>
-														<div>${item.type}</div>
-														<div class="state">${item.state}</div>
-													</div>
-													<div class="popup">
-														<div class="close">
-															<a class="close__img">
-																<img src="./img/close.png" alt="" />
-															</a>
-														</div>
-														<div class="popup__content">
-															<div>Трасса: </div>
-															<div>Каналы: </div>
-															<div>Что-нибудь еще: </div>
-														</div>
-													</div>	
-												</div>`
-		    //вставляем шаблон в страницу(отрисовываем)
+	<div class="table__item">
+	<div>${item.id}</div>
+	<div class="number">${item.number}</div>
+	<div>${item.priority}</div>
+	<div>${item.correspondent}</div>
+	<div>${item.type}</div>
+	<div class="state">${item.state}</div>
+</div>
+<div class="popup__wrapper">
+	<div class="popup">
+		<div class="close">
+			<a class="close__img">
+				<img src="./img/close.png" alt="" / style="width:23px;height:23px;transition:0.4s;">
+				<style>
+				.close__img>img:hover{
+					transform:rotate(360deg);
+					transition:0.4s;
+				}
+				</style>
+			</a>
+		</div>
+		<div class="popup__content">
+			<div>Трасса: 
+			${item.cor1}
+			${item.SL1}
+			${item.cor2}
+			${item.SL2}
+			${item.cor3}
+			${item.SL3}
+			${item.cor4}																
+			</div>
+			<div class="popup__content-down">
+			<div>Загрузка: 
+																		
+			</div>
+			<div class="state"> Состояние: ${item.state}</div
+			</div>
+		</div>
+	</div>	
+</div>			
+</div>`
+    //вставляем шаблон в страницу(отрисовываем)
     systemsContainer.insertAdjacentHTML('beforeend', systemHTML)
   })
 }
